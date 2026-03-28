0bcb822334aaeb07b8a25088e74d03f5e147a8e7
weijianluo114-ux <weijianluo114@gmail.com>
2026-03-28 21:45:10 +0800

    删去部分无用信号的赋值

--- TPU_project/src/rtl/fix_mul_8bits/fix_mul_8bits.v
+++ TPU_project/src/rtl/fix_mul_8bits/fix_mul_8bits.v
@@ -19,7 +19,7 @@
 // Version:                V1.0
 // TEXT NAME:              fix_mul_8bits.v
 // PATH:                   E:\electronic2\project\verilog_pro\TPU_vivado\TPU_project\src\rtl\fix_mul_8bits.v
-// Descriptions:           8bit3级流水线乘法器
+// Descriptions:           8bit4级流水线乘法器
 //                         
 //----------------------------------------------------------------------------------------
 //****************************************************************************************//
@@ -64,8 +64,6 @@ module fix_mul_8bits (
     reg [15:0] pp0_r3, pp1_r3, pp2_r3, pp3_r3;
     reg neg0_r3, neg1_r3, neg2_r3, neg3_r3;
 
-
-
     /******************************* 组合逻辑 ***********************************/
     assign neg_cnt_w4 = neg0_r3 + neg1_r3 + neg2_r3 + neg3_r3;  // 负部分积个数
 
@@ -82,8 +80,6 @@ module fix_mul_8bits (
             enable_r1 <= 1'b1;
         end else begin
             enable_r1 <= 1'b0;
-            // a_r1 <= 8'd0;
-            // b_r1 <= 8'd0;
         end
     end
 
@@ -101,10 +97,6 @@ module fix_mul_8bits (
             enc3_r2 <= enc3_w1;
         end else begin
             enable_r2 <= 1'b0;
-            // enc0_r2   <= 3'd0;
-            // enc1_r2   <= 3'd0;
-            // enc2_r2   <= 3'd0;
-            // enc3_r2   <= 3'd0;
         end
     end
 
@@ -132,14 +124,6 @@ module fix_mul_8bits (
             neg3_r3 <= neg3_w3;
         end else begin
             enable_r3 <= 1'b0;
-            // pp0_r3  <= 16'd0;
-            // pp1_r3  <= 16'd0;
-            // pp2_r3  <= 16'd0;
-            // pp3_r3  <= 16'd0;
-            // neg0_r3 <= 1'b0;
-            // neg1_r3 <= 1'b0;
-            // neg2_r3 <= 1'b0;
-            // neg3_r3 <= 1'b0;
         end
     end
 
