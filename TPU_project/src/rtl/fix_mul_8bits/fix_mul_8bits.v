a1458d6b82a41323f773766da347135630756a40
weijianluo114-ux <weijianluo114@gmail.com>
2026-04-01 16:56:02 +0800

    对8位定点数乘法器的流水线进行优化和改进并进行了参数化

--- TPU_project/src/rtl/fix_mul_8bits/fix_mul_8bits.v
+++ TPU_project/src/rtl/fix_mul_8bits/fix_mul_8bits.v
@@ -24,50 +24,72 @@
 //----------------------------------------------------------------------------------------
 //****************************************************************************************//
 
-module fix_mul_8bits (
-    input       clk,
-    input       rst_n,
-    input       valid_input,
-    input [7:0] a,
-    input [7:0] b,
+module fix_mul_8bits #(
+    parameter WIDTH_INT8 = 8,
+    parameter WIDTH_BOOTH = 3,
+    parameter WIDTH_PRODUCT = 16,
+    parameter WIDTH_PRODUCT_SHIFT = 22,
+    parameter WIDTH_INT32 = 32
+) (
+    input                  clk,
+    input                  rst_n,
+    input                  valid_input,
+    input [WIDTH_INT8-1:0] a,
+    input [WIDTH_INT8-1:0] b,
 
-    output reg        valid_output,  //输出有效
-    output reg [31:0] p              //乘积输出
+    output reg                   valid_output,  //输出有效
+    output reg [WIDTH_INT32-1:0] p              //乘积输出
 );
 
     /******************************* 网表信号 ***********************************/
-    // 第二级：Booth编码
-    wire [2:0] enc0_w1, enc1_w1, enc2_w1, enc3_w1;
+    // 第一级：Booth编码
+    wire [        WIDTH_BOOTH-1:0] enc0_w1;
+    wire [        WIDTH_BOOTH-1:0] enc1_w1;
+    wire [        WIDTH_BOOTH-1:0] enc2_w1;
+    wire [        WIDTH_BOOTH-1:0] enc3_w1;
 
-    // 第三级：部分积生成
-    wire [15:0] pp0_w3, pp1_w3, pp2_w3, pp3_w3;
+    // 第二级：部分积生成
+    wire [      WIDTH_PRODUCT-1:0] pp0_w2;
+    wire [      WIDTH_PRODUCT-1:0] pp1_w2;
+    wire [      WIDTH_PRODUCT-1:0] pp2_w2;
+    wire [      WIDTH_PRODUCT-1:0] pp3_w2;
 
-    // 第四级：压缩树 + 最终加法（组合逻辑）
-    wire [21:0] sum_w4, carry_w4;
-    wire [31:0] product_w4;
+    // 第三级：压缩树 + 最终加法（组合逻辑）
+    wire [WIDTH_PRODUCT_SHIFT-1:0] sum_w3;
+    wire [WIDTH_PRODUCT_SHIFT-1:0] carry_w3;
+    wire [        WIDTH_INT32-1:0] adder_input_a;
+    wire [        WIDTH_INT32-1:0] adder_input_b;
 
     //加法器输入有效信号
-    wire        valid_input_fix_adder;
-    
+    wire                           valid_input_fix_adder;
+
     /******************************* reg信号 ***********************************/
     //第一级输入寄存
-    reg [7:0] a_r1, b_r1;
-    reg       enable_r1;
+    reg  [         WIDTH_INT8-1:0] a_r1;
+    reg  [         WIDTH_INT8-1:0] b_r1;
+    reg                            enable_r1;
 
     // 第二级流水级寄存器
-    reg [7:0] a_r2;
-    reg       enable_r2;
-    reg [2:0] enc0_r2, enc1_r2, enc2_r2, enc3_r2;
+    reg  [         WIDTH_INT8-1:0] a_r2;
+    reg                            enable_r2;
+    reg  [        WIDTH_BOOTH-1:0] enc0_r2;
+    reg  [        WIDTH_BOOTH-1:0] enc1_r2;
+    reg  [        WIDTH_BOOTH-1:0] enc2_r2;
+    reg  [        WIDTH_BOOTH-1:0] enc3_r2;
 
     //第三级流水线
-    reg enable_r3;
-    reg [15:0] pp0_r3, pp1_r3, pp2_r3, pp3_r3;
-
-    //第四级流水线
-    reg enable_r4;
+    reg                            enable_r3;
+    reg  [      WIDTH_PRODUCT-1:0] pp0_r3;
+    reg  [      WIDTH_PRODUCT-1:0] pp1_r3;
+    reg  [      WIDTH_PRODUCT-1:0] pp2_r3;
+    reg  [      WIDTH_PRODUCT-1:0] pp3_r3;
 
     /******************************* 组合逻辑 ***********************************/
+    assign valid_input_fix_adder = enable_r3;
 
+    //将补高位后的值赋值给加法器输入
+    assign adder_input_a = {{(WIDTH_INT32 - WIDTH_PRODUCT_SHIFT) {sum_w3[WIDTH_PRODUCT_SHIFT-1]}}, sum_w3};  //对于sum，不需要左移
+    assign adder_input_b = {{(WIDTH_INT32 - WIDTH_PRODUCT_SHIFT - 1) {sum_w3[WIDTH_PRODUCT_SHIFT-1]}}, sum_w3, 1'b0};  //对于cin，要左移一位
 
     /******************************* 时序逻辑 ***********************************/
     //第一级流水线，首先存储输入
@@ -112,27 +134,12 @@ module fix_mul_8bits (
         end else if (enable_r2) begin
             enable_r3 <= enable_r2;
 
-            pp0_r3 <= pp0_w3;
-            pp1_r3 <= pp1_w3;
-            pp2_r3 <= pp2_w3;
-            pp3_r3 <= pp3_w3;
+            pp0_r3 <= pp0_w2;
+            pp1_r3 <= pp1_w2;
+            pp2_r3 <= pp2_w2;
+            pp3_r3 <= pp3_w2;
         end else begin
             enable_r3 <= 1'b0;
-        end
-    end
-
-    //第4级流水线
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) p <= 0;
-        else if (enable_r3) begin
-            enable_r4 <= 1'b1;
-
-            valid_output <= 1'b1;
-            p <= product_w4;
-        end else begin
-            valid_output <= 1'b0;
-            p <= 32'd0;
-            enable_r4 <= 1'b0;
         end
     end
 
@@ -153,39 +160,32 @@ module fix_mul_8bits (
         .enc1(enc1_r2),
         .enc2(enc2_r2),
         .enc3(enc3_r2),
-        .pp0 (pp0_w3),
-        .pp1 (pp1_w3),
-        .pp2 (pp2_w3),
-        .pp3 (pp3_w3)
+        .pp0 (pp0_w2),
+        .pp1 (pp1_w2),
+        .pp2 (pp2_w2),
+        .pp3 (pp3_w2)
     );
 
-    //第四级：压缩树
+    //第三级：压缩树
     compress_tree_8bits compressor_tree_8bits_inst (
         .pp0  (pp0_r3),
         .pp1  (pp1_r3),
         .pp2  (pp2_r3),
         .pp3  (pp3_r3),
-        .sum  (sum_w4),
-        .carry(carry_w4)
-    );
-
-    //第四级，加法器
-    final_adder final_adder_inst (
-        .sum    (sum_w4),
-        .carry  (carry_w4),
-        .product(product_w4)
+        .sum  (sum_w3),
+        .carry(carry_w3)
     );
 
     //最后使用一个2级流水加法器
     carry_sel_adder_32bit carry_sel_adder_32bit_inst (
         .clk         (clk),
         .valid_input (valid_input_fix_adder),
-        .is_add      (is_add),
-        .a           (a),
-        .b           (b),
-        .sum         (sum),
-        .cout        (cout),
+        .is_add      (1'b1),
+        .a           (adder_input_a),
+        .b           (adder_input_b),
+        .sum         (p),
+        .cout        (),
         .valid_output(valid_output),
-        .overflow    (overflow)
+        .overflow    ()                        //由于最高位是22，故这里相乘不存在溢出
     );
 endmodule
